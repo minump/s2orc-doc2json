24f3a94bd3c1adfa056e7f07b6a88e643f9bf2a0
Minu Mathew <minumpmathews@gmail.com>
2023-04-03 11:17:32 -0500

    changed docker port

--- doc2json/grobid2json/grobid/grobid_client.py
+++ doc2json/grobid2json/grobid/grobid_client.py
@@ -19,7 +19,7 @@ require something scalable too, which is not implemented for the moment.
 
 DEFAULT_GROBID_CONFIG = {
     #"grobid_server": "localhost",
-    "grobid_server": "172.17.0.0/16",
+    "grobid_server": "172.17.0.0",
     "grobid_port": "8070",
     "batch_size": 1000,
     "sleep_time": 5,
